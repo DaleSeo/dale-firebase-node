e32f908d489ceb3a03bba42003608dff28253fc1
서대영/Store기술개발팀/SKP <dale.seo@sk.com>
2016-08-10 10:13:21 +0900

    Develop Todo REST API using Firebase database NodeJS API

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
 # Dale's Firebase & Node
 
+# API Test
+- curl -X GET http://localhost:3000/todos -v
+- curl -X GET http://localhost:3000/todos/-KOjaUiqkAx5OzXzY6sq -v
+- curl -X DELETE http://localhost:3000/todos/-KOlrNLcGD73lFzWj3BD -v
+- curl -X POST http://localhost:3000/todos -d '{"title": "CCS", "done": false}' -H 'content-type:application/json' -v
+- curl -X PUT http://localhost:3000/todos/-KOltUtEgyQXN2FRCSSK -d '{"title": "JavaScript", "done": false}' -H 'content-type:application/json' -v
+
 # Doing
-- Firebase database post
 
 # To do
+- Study Firebase Database REET API
+- Add a HTTP logger
+
 
 # Done
+- Develop Todo REST API using Firebase database NodeJS API
 - ExpressJS setup
--- routes/todos.js
+++ routes/todos.js
@@ -1,17 +1,48 @@
 var express = require('express');
-var router = express.Router();
 var firebase = require('firebase');
+
+var router = express.Router();
 firebase.initializeApp({
   apiKey: "AIzaSyBlxSB7hHcmupywqc_wrNoKoLxuto5arzw",
   authDomain: "first-643b7.firebaseapp.com",
   databaseURL: "https://first-643b7.firebaseio.com",
   storageBucket: "first-643b7.appspot.com"
 });
+firebase.database.enableLogging(true);
+var todoRef = firebase.database().ref('todos');
 
 router.post('/', function(req, res) {
-  var result = firebase.database().ref('todos').push(req.body);
-  console.log(result);
-  res.send(result); // TODO: json parsing error
+  console.log("#req.body : ", req.body)
+  var result = todoRef.push(req.body);
+  console.log(result.key);
+  res.send({key : result.key});
+});
+
+router.get('/', function(req, res) {
+  todoRef.once('value', function(snapshot) {
+    console.log("###" + JSON.stringify(snapshot.val()));
+    res.send(snapshot.val())
+  })
+});
+
+router.get('/:key', function(req, res) {
+  console.log("#req.params.key : " + req.params.key);
+  todoRef.child(req.params.key).once('value', function(snapshot) {
+    console.log("###" + JSON.stringify(snapshot.val()));
+    res.send(snapshot.val())
+  });
+});
+
+router.delete('/:key', function(req, res) {
+  console.log("#req.params.key : " + req.params.key);
+  todoRef.child(req.params.key).remove();
+  res.send({message: 'done'});
+});
+
+router.put('/:key', function(req, res) {
+  console.log("#req.params.key : " + req.params.key);
+  todoRef.child(req.params.key).set(req.body);
+  res.send({message: 'done'});
 });
 
 module.exports = router;
